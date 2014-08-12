11a1e6134cd95d4587a1dd609de2ed208fc97ac5
David Kapp <davidkapp@coshx.com>
2014-08-12 15:13:00 -0400

    update specs to be .json format, since that's what ember will be using

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -5,9 +5,10 @@ RSpec.describe PeopleController, :type => :controller do
 
   describe "GET index" do
     context "user not logged in" do
-      it "redirects the user to sign in" do
-        get :index
-        expect(response).to redirect_to new_user_session_path
+      it "give a not authorized error" do
+        get :index, format: :json
+        expect(response).to_not be_success
+        expect(response.code).to eq("401")
       end
     end
 
@@ -19,7 +20,7 @@ RSpec.describe PeopleController, :type => :controller do
       before { sign_in user }
 
       it "gets the people of the signed in user" do
-        get :index
+        get :index, format: :json
         expect(assigns(:people).count).to eq(2)
         expect(assigns(:people)).to_not include(other_person)
       end
@@ -32,27 +33,27 @@ RSpec.describe PeopleController, :type => :controller do
     let(:person) { create :person, user: user }
 
     it "gets the user specifed" do
-      get :show, id: person.id
+      get :show, format: :json, id: person.id
       expect(assigns(:person)).to eq(person)
     end
   end
 
-  describe "GET edit" do
-    before { sign_in user }
-    let(:person) { create :person, user: user }
+  # describe "GET edit" do
+  #   before { sign_in user }
+  #   let(:person) { create :person, user: user }
 
-    it "finds the correct person to edit" do
-      get :edit, id: person.id
-      expect(assigns(:person)).to eq(person)
-    end
-  end
+  #   it "finds the correct person to edit" do
+  #     get :edit, format: :json, id: person.id
+  #     expect(assigns(:person)).to eq(person)
+  #   end
+  # end
 
   describe "POST create" do
     before { sign_in user }
 
     it "creates a new person for the user" do
       expect {
-        post :create, person: { name: "larry wall", title: "perl dude",
+        post :create, format: :json, person: { name: "larry wall", title: "perl dude",
           description: "I have no idea why Larry Wall is in a Rails test - what the heck?", hour_capacity: 20 }
       }.to change{Person.count}.by(1)
 
@@ -65,7 +66,7 @@ RSpec.describe PeopleController, :type => :controller do
     let(:person) { create :person, user: user }
 
     it "lets you update the person" do
-      put :update, id: person.id, person: { name: "Susan Knowles", title: "CTO" }
+      put :update, format: :json, id: person.id, person: { name: "Susan Knowles", title: "CTO" }
       p = user.people.last
       expect(p.name).to eq("Susan Knowles")
       expect(p.title).to eq("CTO")
@@ -78,7 +79,7 @@ RSpec.describe PeopleController, :type => :controller do
 
     it "lets you delete a person" do
       expect {
-        delete :destroy, id: person.id
+        delete :destroy, format: :json, id: person.id
       }.to change{Person.count}.by(-1)
 
       expect(Person.where(id: person.id)).to be_empty
