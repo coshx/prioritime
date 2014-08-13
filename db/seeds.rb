2e364017cea59074983677183461b409dc6d0e47
Gabe Kopley <gabe@coshx.com>
2014-08-13 11:27:39 -0700

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-owen = User.where(email: "owen@coshx.com").first_or_create(
+owen = User.where(email: "owen@coshx.com").first_or_create!(
   password: "password", password_confirmation: "password"
 )
 
 %w{Ben Chielo Dave Gil Josh Michael Sang}.each do |name|
-  Person.where(name: name).first_or_create(
-    title: name == "Gil" ? "Designer" : "Developer", hour_capacity: 40
+  Person.where(name: name).first_or_create!(
+    title: name == "Gil" ? "Designer" : "Developer", hour_capacity: 40, user_id: owen.id
   )
 end
 
 ["Couchsurfing", "Search Lateral", "ACAC", "Orbit Works", "Minr"].each do |name|
-  Project.where(name: name).first_or_create(
+  Project.where(name: name).first_or_create!(
     client: name, user_id: owen.id
   )
 end
