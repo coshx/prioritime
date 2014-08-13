338db2fe1477676c705ff6d8b28d11c3ca88197c
Gabe Kopley <gabe@coshx.com>
2014-08-13 12:16:39 -0700

    More seed assignments

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ owen = User.where(email: "owen@coshx.com").first_or_create!(
   password: "password", password_confirmation: "password"
 )
 
-%w{Ben Chielo Dave Gil Josh Michael Sang}.each do |name|
+%w{Ben Chielo Dave Gabe Gil Josh Michael Sang}.each do |name|
   Person.where(name: name).first_or_create!(
     title: name == "Gil" ? "Designer" : "Developer", hour_capacity: 40, user_id: owen.id
   )
@@ -22,13 +22,35 @@ end
   )
 end
 
+start_monday = Chronic.parse("last monday").to_date
+
+dave = Person.find_by_name("Dave")
+couchsurfing = Project.find_by_name("Couchsurfing")
+
+dave_cs = ProjectAssignment.where(project_id: couchsurfing.id, person_id: dave.id)
+  .first_or_create!(summary: "a couple days a week indefinitely")
+
+10.times do |i|
+  WeeklyProjectAssignment.where(project_assignment_id: dave_cs.id, week_start: start_monday + i.weeks)
+    .first_or_create!(days_working: 5)
+end
+
+gabe = Person.find_by_name("Gabe")
+search_lateral = Project.find_by_name("Search Lateral")
+
+gabe_sl = ProjectAssignment.where(project_id: search_lateral.id, person_id: gabe.id)
+  .first_or_create!(summary: "a couple days a week indefinitely")
+
+10.times do |i|
+  WeeklyProjectAssignment.where(project_assignment_id: gabe_sl.id, week_start: start_monday + i.weeks)
+    .first_or_create!(days_working: 2)
+end
+
 sang = Person.find_by_name("Sang")
 acac = Project.find_by_name("ACAC")
 
 sang_acac = ProjectAssignment.where(project_id: acac.id, person_id: sang.id)
   .first_or_create!(summary: "35 hours/week for 10 weeks")
-
-start_monday =  Chronic.parse("last monday").to_date
 
 10.times do |i|
   WeeklyProjectAssignment.where(project_assignment_id: sang_acac.id, week_start: start_monday + i.weeks)
