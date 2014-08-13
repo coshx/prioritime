68972d7fc25df4f8a219d40ed72436ed97914b3d
Sang Won Seo <sangseo89@gmail.com>
2014-08-13 14:25:07 -0400

    replaces weekly project assignments controller spec with weekly project assignments request spec

--- app/controllers/weekly_project_assignments_controller.rb
+++ app/controllers/weekly_project_assignments_controller.rb
@@ -1,76 +1,44 @@
 class WeeklyProjectAssignmentsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user_from_token!
   before_action :set_weekly_project_assignment, only: [:show, :edit, :update, :destroy]
 
-  # GET /weekly_project_assignments
   # GET /weekly_project_assignments.json
   def index
     @weekly_project_assignments = WeeklyProjectAssignment.for_user(current_user)
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @weekly_project_assignments }
-    end
+    render json: @weekly_project_assignments
   end
 
-  # GET /weekly_project_assignments/1
   # GET /weekly_project_assignments/1.json
   def show
-    respond_to do |format|
-      format.html
-      format.json { render json: @weekly_project_assignment }
-    end
+    render json: @weekly_project_assignment
   end
 
-  # GET /weekly_project_assignments/new
-  def new
-    @weekly_project_assignment = WeeklyProjectAssignment.new
-  end
-
-  # GET /weekly_project_assignments/1/edit
-  def edit
-  end
-
-  # POST /weekly_project_assignments
   # POST /weekly_project_assignments.json
   def create
     project_assignment = ProjectAssignment.for_user(current_user).find(params[:weekly_project_assignment][:project_assignment_id])
     @weekly_project_assignment = WeeklyProjectAssignment.new(weekly_project_assignment_params)
     @weekly_project_assignment.project_assignment = project_assignment
 
-    respond_to do |format|
-      if @weekly_project_assignment.save
-        format.html { redirect_to @weekly_project_assignment, notice: 'Weekly project assignment was successfully created.' }
-        format.json { render json: @weekly_project_assignment, status: :created, location: @weekly_project_assignment }
-      else
-        format.html { render :new }
-        format.json { render json: @weekly_project_assignment.errors, status: :unprocessable_entity }
-      end
+    if @weekly_project_assignment.save
+      render json: @weekly_project_assignment, status: :created
+    else
+      render json: @weekly_project_assignment.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /weekly_project_assignments/1
   # PATCH/PUT /weekly_project_assignments/1.json
   def update
-    respond_to do |format|
-      if @weekly_project_assignment.update(weekly_project_assignment_params)
-        format.html { redirect_to @weekly_project_assignment, notice: 'Weekly project assignment was successfully updated.' }
-        format.json { render json: @weekly_project_assignment, status: :ok, location: @weekly_project_assignment }
-      else
-        format.html { render :edit }
-        format.json { render json: @weekly_project_assignment.errors, status: :unprocessable_entity }
-      end
+    if @weekly_project_assignment.update(weekly_project_assignment_params)
+      render json: @weekly_project_assignment, status: 200
+    else
+      render json: @weekly_project_assignment.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /weekly_project_assignments/1
   # DELETE /weekly_project_assignments/1.json
   def destroy
     @weekly_project_assignment.destroy
-    respond_to do |format|
-      format.html { redirect_to weekly_project_assignments_url, notice: 'Weekly project assignment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    head :no_content
   end
 
   private
