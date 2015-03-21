85ea47bc21f43a20ed84fa141dffe01a9c5f109c
Brad Johnson <cdale77@gmail.com>
2015-03-20 17:32:47 -0700

    refactor projects request spec

--- spec/requests/projects_request_spec.rb
+++ spec/requests/projects_request_spec.rb
@@ -1,102 +1,116 @@
 require 'rails_helper'
 
 describe ProjectsController do
-  let(:user) { create :user }
+  let!(:organization) { create(:organization) }
+  let!(:user) { create(:user) }
+  let!(:admin) { Admin.create(organization: organization, user: user) }
 
   describe "GET index" do
-    let(:route) { { type: :get, url: projects_path } }
+    let!(:project1) { create :project, organization: organization }
+    let!(:project2) { create :project, organization: organization }
+    let!(:other_project) { create :project }
+    let(:route) { { type: :get, url: projects_path(organization) } }
 
-    it_behaves_like :authentication_required_route
+    before do
+      get(route[:url], {}, authenticated_json_header(user))
+    end
 
+    context "authentication" do
+      it_behaves_like :authentication_required_route
+    end
 
-    context "user signed in" do
-      let!(:project1) { create :project, user: user }
-      let!(:project2) { create :project, user: user }
-      let!(:other_project) { create :project }
-
+    context "when the user is authorized" do
       it "gets the projects of the signed in user" do
-        get(route[:url], {}, authenticated_json_header(user))
-
         expect(response).to be_success
         expect(response).to be_in_json
-
-        projects_response = json(response.body)[:projects]
-        expect(projects_response.count).to eq(2)
-        expect(projects_response.first[:id]).to eq(project2.id)
-        expect(projects_response.last[:id]).to eq(project1.id)
+        project_ids = json(response.body)[:projects].map { |p| p[:id] }
+        expect(project_ids). to eq [project1.id, project2.id]
       end
     end
   end
 
   describe "GET show" do
-    let(:project) { create :project, user: user }
-    let(:route) { { type: :get, url: project_path(project)}}
+    let!(:project) { create :project, organization: organization }
+    let(:route) { { type: :get, url: project_path(organization, project)} }
 
-    it_behaves_like :authentication_required_route
-
-    it "gets the user specifed" do
+    before do
       get(route[:url], {}, authenticated_json_header(user))
+    end
 
-      expect(response).to be_success
-      expect(response).to be_in_json
+    context "authentication" do
+      it_behaves_like :authentication_required_route
+    end
 
-      project_response = json(response.body)[:project]
-      expect(project_response[:id]).to eql(project.id)
+    context "when the user is authorized" do
+      it "should return the correct project" do
+        expect(response).to be_success
+        expect(response).to be_in_json
+        expect(json(response.body)[:project][:id]).to eq project.id
+      end
     end
   end
 
   describe "POST create" do
-    let(:route) { { type: :post, url: projects_path } }
+    let(:route) { { type: :post, url: projects_path(organization) } }
+    let!(:new_project) { create(:project, organization: organization) }
+    let(:post_params) { { project: new_project.attributes } }
 
-    it_behaves_like :authentication_required_route
+    before do
+      post(route[:url], post_params, authenticated_json_header(user))
+    end
 
-    it "creates a new project for the user" do
-      post(route[:url], { project: { name: "Big Money", duration_weeks: "8",
-          description: "Probably will make lots of money" } }, 
-          authenticated_json_header(user))
+    context "authentication" do
+      it_behaves_like :authentication_required_route
+    end
 
-      expect(response).to be_success
-      expect(response).to be_in_json
-
-      new_project_response = json(response.body)[:project]
-      expect(new_project_response[:name]).to eql("Big Money")
-      expect(user.projects.last.name).to eql("Big Money")      
+    context "when the user is authorized" do
+      it "should create a new project" do
+        expect(response).to be_success
+        expect(response).to be_in_json
+        expect(json(response.body)[:project][:name]).to eq new_project.name
+      end
     end
   end
 
   describe "PUT update" do
-    let(:project) { create :project, user: user }
-    let(:route) { { type: :patch, url: project_path(project) } }
+    let!(:project) { create :project, organization: organization }
+    let(:route) { { type: :patch, url: project_path(organization, project) } }
 
-    it_behaves_like :authentication_required_route
-
-    it "lets you update the project" do
-      patch(route[:url], { project: { name: "Strange Project" } }, 
+    before do
+      patch(route[:url], { project: { name: "Strange Project" } },
         authenticated_json_header(user))
+    end
 
-      expect(response).to be_success
-      expect(response).to be_in_json
+    context "authentication" do
+      it_behaves_like :authentication_required_route
+    end
 
-      updated_project_response = json(response.body)[:project]
-      expect(updated_project_response[:name]).to eql("Strange Project")
-
-      p = user.projects.last
-      expect(p.name).to eq("Strange Project")
+    context "when the user is authorized" do
+      it "updates the project" do
+        expect(response).to be_success
+        expect(response).to be_in_json
+        expect(json(response.body)[:project][:name]).to eq "Strange Project"
+      end
     end
   end
 
   describe "DELETE destroy" do
-    let(:route) { { type: :delete, url: project_path(id: project.id) } }
-    let!(:project) { create :project, user: user }
+    let!(:project) { create :project, organization: organization }
+    let(:route) { { type: :delete, url: project_path(organization, project) } }
 
-    it_behaves_like :authentication_required_route
-    
-    it "lets you delete a project" do
+    before do
       delete(route[:url], {}, authenticated_json_header(user))
+    end
 
-      expect(response).to be_success
+    context "authentication" do
+      it_behaves_like :authentication_required_route
+    end
 
-      expect(Project.where(id: project.id)).to be_empty
+    context "when the user is authorized" do
+      it "should destroy the project" do
+        expect(response).to be_success
+        expect(Project.where(id: project.id)).to be_empty
+      end
     end
   end
 end
